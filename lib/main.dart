8564066c81761faf9e015dc92b25ee8515b06aa0
hami-r <mohammed.hamir@nestgroup.net>
2024-01-10 17:38:18 +0530

    removed firebase settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:expense_app/pages/home_page.dart';
 import 'package:expense_app/providers/expense_provider.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
